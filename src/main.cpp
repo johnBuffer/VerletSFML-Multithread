6f12798bcae58b91b816c0d26473490e3480754e
Jean Tampon <jean.tampon@gmail.com>
2023-11-06 18:56:19 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main()
     // Main loop
     const float dt = 1.0f / static_cast<float>(fps_cap);
     while (app.run()) {
-        if (solver.objects.size() < 80000 && emit) {
+        if (solver.objects.size() < 16000 && emit) {
             for (uint32_t i{20}; i--;) {
                 const auto id = solver.createObject({2.0f, 10.0f + 1.1f * i});
                 solver.objects[id].last_position.x -= 0.2f;
@@ -49,7 +49,9 @@ int main()
             }
         }
 
+        sf::Clock clock;
         solver.update(dt);
+        std::cout << clock.getElapsedTime().asMicroseconds() << std::endl;
 
         render_context.clear();
         renderer.render(render_context);
